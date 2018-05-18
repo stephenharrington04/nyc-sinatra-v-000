dd54f9f340c110220e01a1bf1feaf56caf680d21
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-18 04:59:14 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,6 +9,18 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/edit'
+  end
+
   post '/figures' do
     @figure = Figure.create(params[:figure])
     if !params[:title].empty?
@@ -22,21 +34,8 @@ class FiguresController < ApplicationController
     redirect "/figures/#{@figure.id}"
   end
 
-  get '/figures/:id' do
-    @figure = Figure.find_by(id: params[:id])
-
-    erb :'/figures/show'
-  end
-
-  get '/figures/:id/edit' do
-    @figure = Figure.find_by(id: params[:id])
-
-    erb :'/figures/edit'
-  end
-
   patch '/figures/:id' do
-    @figure = Figure.find_by(id: params[:id])
-    binding.pry
+    @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
 
     if !params[:title].empty?
